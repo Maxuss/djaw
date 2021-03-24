8b02b351c56e1b1683a23f71a9e757487c5db3a8
Maksim <maxus.info.mail@gmail.com>
2021-03-24 19:57:15 +0300

    pre pre pre pre pre pre pre pre pre pre pre pre pre pre super alpha 0.000.000.000.000.001

---
+++ max/src/com/maxus/djaw/DJaw.java
@@ -0,0 +1,34 @@
+package com.maxus.djaw;
+
+
+
+// main class
+public class DJaw {
+
+    // starting
+    public static void main(String args[]){
+        DJMessage("hello world", 0);
+    }
+    //method for debug messages
+    public static void DJMessage(String message, int type /* 0 for debug, 1 for warn, 2 for error, 3 for fatal*/){
+        String msg_returner;
+        switch (type) {
+            case(0):
+                msg_returner = "[DJAW] -DEBUG > " + message;
+                break;
+            case(1):
+                msg_returner = "[DJAW] -WARNING > " + message;
+                break;
+            case(2):
+                msg_returner = "[DJAW] -ERROR > " + message;
+                break;
+            case(3):
+                msg_returner = "[DJAW] -FATAL_ERROR > " + message;
+                break;
+            default:
+                msg_returner = "[DJAW] > " + message;
+                break;
+        }
+        System.out.println(msg_returner);
+    }
+}
